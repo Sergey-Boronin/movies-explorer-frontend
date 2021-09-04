9e9bd39ac548702f65250c6f9a084a2bb2e7dd4e
Sergey Boronin <bo-ronin@yandex.ru>
2021-09-04 11:33:12 +0300

    restore after git crash

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -37,9 +37,9 @@ function Footer() {
           <div className = 'footer__container'>
             <p className = 'footer__date'>&copy; 2021</p>
             <div className = 'footer__links'>
-            <Link href = 'https://praktikum.yandex.ru/' className = 'footer__link' target='_blank' rel='noreferrer'>Яндекс.Практикум</Link>
-              <Link href = 'https://github.com/Sergey-Boronin/' className = 'footer__link' target='_blank' rel='noreferrer'>Github</Link>
-              <Link href = 'https://www.instagram.com/bo_ronin/' className = 'footer__link' target='_blank' rel='noreferrer'>Instagram</Link>
+            <a href = 'https://praktikum.yandex.ru/' className = 'footer__link' target='_blank' rel='noreferrer'>Яндекс.Практикум</a>
+              <a href = 'https://github.com/Sergey-Boronin/' className = 'footer__link' target='_blank' rel='noreferrer'>Github</a>
+              <a href = 'https://www.instagram.com/bo_ronin/' className = 'footer__link' target='_blank' rel='noreferrer'>Instagram</a>
             </div>
           </div>
         </div>
